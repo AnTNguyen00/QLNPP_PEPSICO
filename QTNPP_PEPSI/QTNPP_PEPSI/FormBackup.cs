d08438472322c2dd8b98a21aa2b8c7ed38fe183d
mynguyen11 <nguyenngocmy11102000@gmail.com>
2022-11-26 11:58:43 +0700

    task my

---
+++ QTNPP_PEPSI/QTNPP_PEPSI/FormBackup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QTNPP_PEPSI
+{
+    public partial class FormBackup : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source = DESKTOP-NOHK2T0\\MYNGUYEN; Initial Catalog = QL_QTNPP_PEPSI; User ID = sa; Password = sa2012");
+
+        public FormBackup()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtLocation.Text = dlg.SelectedPath;
+                btnBackup.Enabled = true;
+            }
+        }
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            string database = con.Database.ToString();
+
+            try
+            {
+                if (txtLocation.Text == string.Empty)
+                    MessageBox.Show("Vui lòng chọn vị trí file backup!");
+                else
+                {
+                    string str = "BACKUP DATABASE [" + database + "] TO DISK = '" + txtLocation.Text + "\\" + "QL_QTNPP_PEPSI" + "-" + DateTime.Now.ToString("dd-MM-yyyy--HH-mm-ss") + ".bak'";
+
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(str, con);
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Sao lưu dữ liệu thành công");
+                    con.Close();
+                    btnBackup.Enabled = false;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Sao lưu dữ liệu không thành công!");
+            }
+        }
+    }
+}
